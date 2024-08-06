2c4283ae2355a0f7d2d303ba47c338d31dc89262
d4lion <d4lion.dev@gmail.com>
2024-08-05 22:53:23 -0500

    :memo: feat: Add questions.js file for example data

---
+++ data/questions.js
@@ -0,0 +1,24 @@
+// Todas las preguntas deben ser referentes a viajes y destinos turísticos.
+
+export const preguntas = [
+  {
+    question: "¿A donde te gustaria ir?",
+    answers: ["Cancun", "Caracas", "Panama", "Bogota"],
+  },
+  {
+    question: "¿Cual de estos climas prefieres?",
+    answers: ["Frio", "Calor", "Templado"],
+  },
+  {
+    question: "¿Que tipo de comida te gusta?",
+    answers: ["Mexicana", "Venezolana", "Panameña"],
+  },
+  {
+    question: "¿Que tipo de actividades prefieres?",
+    answers: ["Acuaticas", "Montañismo", "Culturales"],
+  },
+  {
+    question: "¿Que tipo de alojamiento prefieres?",
+    answers: ["Hotel", "Hostal", "Casa"],
+  },
+]
